e7be1ab0fce81509050b362a352079290759c048
DanielLin9406 <universetennis@gmail.com>
2021-03-12 23:12:38 +0800

    updated

--- Datastructure/TernarySearchTree.py
+++ Datastructure/TernarySearchTree.py
@@ -114,5 +114,13 @@ word_list = [
 ]
 
 t = AutoCompleteTree(word_list)
-pprint(t.n.equal.equal.value)
-print(t.find(pattern))
+pprint(t.n.right.right.value)  # c
+pprint(t.n.right.right.equal.right.value)
+
+pprint(t.n.right.right.value)  # c
+pprint(t.n.right.right.equal.value)  # a
+pprint(t.n.right.right.equal.equal.value)  # r
+# pprint(t.n.middle.right.right.value)
+# pprint(t.n.right.right.value)
+# pprint(t.n.equal.equal.value)
+# print(t.find(pattern))
